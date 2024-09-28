a3c33222280fef4034c7ff92f87baa58af523060
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-09-27 20:46:08 -0500

    Proyecto creado

---
+++ src/main/java/com/example/app_hfc_rabbitmq_web/AppHfcRabbitmqWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_hfc_rabbitmq_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHfcRabbitmqWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHfcRabbitmqWebApplication.class, args);
+	}
+
+}
